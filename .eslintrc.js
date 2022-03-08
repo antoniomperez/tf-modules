fef9815afd87036289dd1b1a0bc36a9c06b1eebf
Antonio Márquez Pérez <antonio.marquezperez@everis.nttdata.com>
2022-03-08 15:22:31 +0100

    chore: add prettier configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,16 +4,23 @@ module.exports = {
     es2021: true,
   },
   extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
     'airbnb-base',
+    'plugin:prettier/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: [
-    '@typescript-eslint',
-  ],
+  plugins: ['@typescript-eslint', 'prettier', 'import'],
   rules: {
+    'prettier/prettier': 'error',
+    'import/extensions': 'off',
+    'no-console': 'off',
   },
 };
