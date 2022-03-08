f07f24f7d976be23c74a500d8f6940c964a5316f
Antonio Márquez Pérez <antonio.marquezperez@everis.nttdata.com>
2022-03-08 16:03:53 +0100

    chore: configure package to be coded

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  injectGlobals: true,
+  testMatch: ['**/*.test.ts'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  verbose: true,
 };
