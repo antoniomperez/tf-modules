0c9a1797391c4f0e2c6b1ce64c0b80ef92723d61
Antonio Márquez Pérez <antonio.marquezperez@everis.nttdata.com>
2022-03-10 10:29:41 +0100

    test: add dev stage inside module

---
+++ aws/terraform-remote-state-s3/dev/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  # backend "s3" {}
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+  profile = var.profile
+}
+
+# Module
+module "terraform_remote_state_s3" {
+  source = "../"
+
+  providers = {
+    aws = aws
+  }
+
+  kms_encryption = true
+  remote_lock    = true
+}
