4e80853422cf0d36056559f2c45e570796c87efc
Antonio Márquez Pérez <antonio.marquezperez@everis.nttdata.com>
2022-03-11 09:08:17 +0100

    chore(terraform-cloud): add scope in conventional commit configuration

--- config/cz-config.js
+++ config/cz-config.js
@@ -1,43 +1,43 @@
 module.exports = {
   types: [
-    { value: "feat", name: "feat:     A new feature" },
-    { value: "fix", name: "fix:      A bug fix" },
-    { value: "docs", name: "docs:     Documentation only changes" },
+    { value: 'feat', name: 'feat:     A new feature' },
+    { value: 'fix', name: 'fix:      A bug fix' },
+    { value: 'docs', name: 'docs:     Documentation only changes' },
     {
-      value: "style",
-      name: "style:    Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)",
+      value: 'style',
+      name: 'style:    Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)',
     },
     {
-      value: "refactor",
-      name: "refactor: A code change that neither fixes a bug nor adds a feature",
+      value: 'refactor',
+      name: 'refactor: A code change that neither fixes a bug nor adds a feature',
     },
     {
-      value: "perf",
-      name: "perf:     A code change that improves performance",
+      value: 'perf',
+      name: 'perf:     A code change that improves performance',
     },
-    { value: "test", name: "test:     Adding missing tests" },
+    { value: 'test', name: 'test:     Adding missing tests' },
     {
-      value: "ci",
-      name: "ci:       Change to our CI configuration files and scripts (example scopes: Travis, Github Actions, Circle CI)",
-    },
-    {
-      value: "build",
-      name: "build:    Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)",
+      value: 'ci',
+      name: 'ci:       Change to our CI configuration files and scripts (example scopes: Travis, Github Actions, Circle CI)',
     },
     {
-      value: "chore",
-      name: "chore:    Changes to the build process or auxiliary tools\n            and libraries such as documentation generation",
+      value: 'build',
+      name: 'build:    Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)',
     },
-    { value: "revert", name: "revert:   Revert to a commit" },
-    { value: "WIP", name: "WIP:      Work in progress" },
+    {
+      value: 'chore',
+      name: 'chore:    Changes to the build process or auxiliary tools\n            and libraries such as documentation generation',
+    },
+    { value: 'revert', name: 'revert:   Revert to a commit' },
+    { value: 'WIP', name: 'WIP:      Work in progress' },
   ],
 
-  scopes: [{ name: "actions" }],
+  scopes: [{ name: 'actions' }, { name: 'terraform-cloud' }],
 
   allowTicketNumber: true,
   isTicketNumberRequired: false,
-  ticketNumberPrefix: "#",
-  ticketNumberRegExp: "\\d{1,5}",
+  ticketNumberPrefix: '#',
+  ticketNumberRegExp: '\\d{1,5}',
 
   // it needs to match the value for field type. Eg.: 'fix'
   /*
@@ -53,21 +53,21 @@ module.exports = {
   // override the messages, defaults are as follows
   messages: {
     type: "Select the type of change that you're committing:",
-    scope: "\nDenote the SCOPE of this change (optional):",
+    scope: '\nDenote the SCOPE of this change (optional):',
     // used if allowCustomScopes is true
-    customScope: "Denote the SCOPE of this change:",
-    subject: "Write a SHORT, IMPERATIVE tense description of the change:\n",
+    customScope: 'Denote the SCOPE of this change:',
+    subject: 'Write a SHORT, IMPERATIVE tense description of the change:\n',
     body: 'Provide a LONGER description of the change (optional). Use "|" to break new line:\n',
-    breaking: "List any BREAKING CHANGES (optional):\n",
+    breaking: 'List any BREAKING CHANGES (optional):\n',
     footer:
-      "List any ISSUES CLOSED by this change (optional). E.g.: #31, #34:\n",
-    confirmCommit: "Are you sure you want to proceed with the commit above?",
+      'List any ISSUES CLOSED by this change (optional). E.g.: #31, #34:\n',
+    confirmCommit: 'Are you sure you want to proceed with the commit above?',
   },
 
   allowCustomScopes: true,
-  allowBreakingChanges: ["feat", "fix"],
+  allowBreakingChanges: ['feat', 'fix'],
   // skip any questions you want
-  skipQuestions: ["body"],
+  skipQuestions: ['body'],
 
   // limit subject length
   subjectLimit: 100,
